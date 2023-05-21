bc09e7fa46598240d81ea9c617c2c47cde3cb33c
Caio Gonzales <35455561+caiogonzales@users.noreply.github.com>
2023-05-21 20:54:51 -0300

    ajustes css

--- assets/countdown.js
+++ assets/countdown.js
@@ -16,8 +16,12 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  //document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  document.getElementById("days").innerHTML = days + " dias";
+  document.getElementById("hours").innerHTML = hours + " horas";
+  document.getElementById("minutes").innerHTML = minutes + " minutos";
+  document.getElementById("seconds").innerHTML = seconds;
     
   // If the count down is over, write some text 
   if (distance < 0) {
